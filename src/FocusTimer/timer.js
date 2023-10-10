121dac115f66656c067406513fdfdfdc2f045d60
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-10 18:10:45 -0300

    new commit

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,3 +1,20 @@
-export function updateDisplay(minutes, seconds) {
+import state from './state.js';
+import * as el from './elements.js';
 
+export function countDown() {
+    if(!state.isRunning) {
+        return
+    }
+
+    console.log('iniciou')
+}
+
+export function updateDisplay(minutes, seconds) {
+    minutes = minutes ?? state.minutes;
+    seconds = seconds ?? state.seconds;
+
+    el.minutes.textContent = String(minutes).
+    padStart(2, "0");
+    el.seconds.textContent = String(seconds).
+    padStart(2, "0");
 };
